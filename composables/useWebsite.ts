b0f8b29a06b3d92a88e8c35976907f11be32e0dd
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-01-09 12:07:36 +0000

    Store For Navigation

---
+++ composables/useWebsite.ts
@@ -0,0 +1,30 @@
+export const useWebsiteStore = defineStore('websiteStore', {
+    state: () => ({
+      menuItems: [
+        {
+            name: 'Home',
+            link: '/home'
+        },
+        {
+            name: 'About',
+            link: '/about'
+        },
+        {
+            name: 'Blog',
+            link: '/blog'
+        },
+        {
+            name: 'Contact Us',
+            link: '/contact-us'
+        }
+      ],
+      description: ''
+    }),
+    actions: {
+      async fetch() {
+        const infos = await $fetch('https://api.nuxt.com/modules/pinia')
+  
+      }
+    }
+  })
+  
