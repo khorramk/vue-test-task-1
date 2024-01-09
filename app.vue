151c89da6d6e4c21ae5db255c020707bfc25b1d1
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-01-09 19:27:51 +0000

    Person Selection Form: html created

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
     <app-header></app-header>
     <app-cta></app-cta>
     <app-learn-more-cta-section></app-learn-more-cta-section>
+    <app-person-type-selection></app-person-type-selection>
     <app-feature-section></app-feature-section>
   </main>
 </template>
