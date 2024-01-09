23d225ae3e697599efe9196ee46cfc472b697b83
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-01-09 18:13:56 +0000

    CTA section: Replaced img with NuxtImg

--- components/AppLearnMoreCtaSection.vue
+++ components/AppLearnMoreCtaSection.vue
@@ -1,17 +1,21 @@
 <template>
-    <section class="cta-section">
-        <div class="cta-content">
-            <div class="cta-image">
-                <!-- Replace 'your-image.jpg' with your actual image URL -->
-                <picture>
-                    <img src="/img/ctaLearnMore.svg" style="width: 50%;" alt="">
-                </picture>
-            </div>
-            <div class="cta-text">
-                <h2></h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae fugiat expedita asperiores sapiente architecto, minus dolor voluptatum dicta, obcaecati tenetur ipsum repellendus aliquid aspernatur dolore facere labore totam distinctio. Ex!</p>
-                <a href="#" class="cta-button">Learn more</a>
-            </div>
-        </div>
-    </section>
+  <section class="cta-section">
+    <div class="cta-content">
+      <div class="cta-image">
+        <NuxtImg src="/img/ctaLearnMore.svg"/>
+      </div>
+      <div class="cta-text">
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+        </h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          fugiat expedita asperiores sapiente architecto, minus dolor voluptatum
+          dicta, obcaecati tenetur ipsum repellendus aliquid aspernatur dolore
+          facere labore totam distinctio. Ex!
+        </p>
+        <a href="#" class="cta-button">Learn more</a>
+      </div>
+    </div>
+  </section>
 </template>
