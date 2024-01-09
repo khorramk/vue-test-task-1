f69f62889ba0c03e095c55f3fb03770fb92faaba
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-01-09 18:56:27 +0000

    Feature section: box of links features

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
     <app-header></app-header>
     <app-cta></app-cta>
     <app-learn-more-cta-section></app-learn-more-cta-section>
+    <app-feature-section></app-feature-section>
   </main>
 </template>
 <style lang="scss">
---
+++ components/AppFeatureSection.vue
@@ -0,0 +1,51 @@
+<template>
+  <section id="features" class="features-section">
+    <div class="section-content">
+      <h2>Key Features</h2>
+      
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates,
+        ipsam nulla deserunt doloremque autem hic consequatur, possimus numquam
+        eos nihil molestias beatae temporibus maiores delectus ratione harum
+        dolorem, eum in?
+      </p>
+
+      <div class="feature-container">
+        <div class="feature">
+          <h3>Feature 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id
+            elit nec arcu cursus aliquam.
+          </p>
+        </div>
+
+        <div class="feature">
+          <h3>Feature 2</h3>
+          <p>
+            Curabitur suscipit orci vel turpis fermentum, ut fermentum quam
+            cursus. Duis et mauris vitae velit venenatis accumsan vel nec dui.
+          </p>
+          <a href="#">Learn more</a>
+        </div>
+
+        <div class="feature">
+          <h3>Feature 3</h3>
+          <p>
+            Curabitur suscipit orci vel turpis fermentum, ut fermentum quam
+            cursus. Duis et mauris vitae velit venenatis accumsan vel nec dui.
+          </p>
+          <a href="#">Learn more</a>
+        </div>
+
+        <div class="feature">
+          <h3>Feature 4</h3>
+          <p>
+            Curabitur suscipit orci vel turpis fermentum, ut fermentum quam
+            cursus. Duis et mauris vitae velit venenatis accumsan vel nec dui.
+          </p>
+          <a href="#">Learn more</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
