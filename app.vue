b715556e42f30938f26240dc0e68ff83d34f166a
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-01-09 19:57:46 +0000

    Layouts: folder added

--- app.vue
+++ app.vue
@@ -1,11 +1,7 @@
 <template>
-  <main>
-    <app-header></app-header>
-    <app-cta></app-cta>
-    <app-learn-more-cta-section></app-learn-more-cta-section>
-    <app-person-type-selection></app-person-type-selection>
-    <app-feature-section></app-feature-section>
-  </main>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
 <style lang="scss">
 @use "~/assets/scss/main.scss";
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <app-header></app-header>
+    <slot />
+  </main>
+</template>
