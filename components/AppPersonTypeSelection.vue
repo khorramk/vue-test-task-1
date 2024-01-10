a8d254b9cc16f90d5f859697ceef16fc79d4eaf3
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-01-10 20:52:16 +0000

    Selection Form: Completed

--- components/AppPersonTypeSelection.vue
+++ components/AppPersonTypeSelection.vue
@@ -1,22 +1,40 @@
 <template>
-    <section>
-        <div class="container">
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit neque, unde perferendis totam qui saepe at. Perferendis iusto excepturi iste. Quaerat aliquam culpa excepturi expedita incidunt quod omnis distinctio quia.
-            </h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id corrupti amet accusantium doloremque vitae temporibus, eligendi sunt aperiam reiciendis vero quasi assumenda, quo minima inventore voluptates neque accusamus ratione fuga?</p>
-        </div>
-        <div class="selection-form">
-            <form action="" method="post">
-                <span>I am</span>
-                <select name="" id="">
-                    <option value="">an individual</option>
-                </select>
-                <span>and I</span>
-                <select name="" id="">
-                    <option value="">want to learn</option>
-                </select>
-                <button type="submit">Start now</button>
-            </form>
-        </div>
-    </section>
+  <section>
+    <div class="selection-type-container">
+      <div class="selection-type-intro">
+        <h3>
+          How can we help you?
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id corrupti
+          amet accusantium 
+        </p>
+      </div>
+      <div class="selection-form">
+        <form action="" method="post">
+          <div class="selection-form__container">
+            <div class="selection-begins">
+              <span>I am</span>
+              <select name="" id="" class="selection">
+                <option value="">an individual</option>
+              </select>
+            </div>
+            <div class="selection-joiner">
+              <span>and</span>
+            </div>
+            <div class="selection-ends">
+              <span>I want </span>
+
+              <select name="" id="" class="selection">
+                <option value="">want to learn</option>
+              </select>
+              <button type="submit" class="selection__submit-btn">
+                Start now
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
